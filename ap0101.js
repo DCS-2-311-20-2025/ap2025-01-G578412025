ae557df7881db3064165f7302200375db1aa453e
student <student@loaner16.local>
2025-10-01 14:59:26 +0900

    kadaiTeisyutu01

--- ap0101.js
+++ ap0101.js
@@ -14,21 +14,48 @@ function init() {
   const axes = new THREE.AxesHelper(18);
   scene.add(axes);
 
-  /* 平面の設定
+  // 平面の設定
   const planeGeometry = new THREE.PlaneGeometry(32, 16);
   const planeMaterial = new THREE.MeshBasicMaterial({ color: 0x606060});
   const plane = new THREE.Mesh(planeGeometry, planeMaterial);
   plane.rotation.x = -0.5 * Math.PI;
   scene.add(plane);
-  /**/
+
+  //立体[箱1]の設定
+  const box1Geometry = new THREE.BoxGeometry(8,8,8);
+  const box1Material = new THREE.MeshLambertMaterial({color:0x008060});
+  const box1 = new THREE.Mesh(box1Geometry,box1Material);
+  box1.position.y = 4;
+  scene.add(box1);
+  
+  //立体[箱2]の設定
+  const box2Geometry = new THREE.BoxGeometry(8,4,2);
+  const box2Material = new THREE.MeshLambertMaterial({color:0xFF8000});
+  const box2 = new THREE.Mesh(box2Geometry,box2Material);
+  box2.position.x = 10;
+  box2.position.y = 3;
+  scene.add(box2);
+
+  //立体[箱2]の設定
+  const box3Geometry = new THREE.BoxGeometry(1,6,10);
+  const box3Material = new THREE.MeshLambertMaterial({color:0x098AA3});
+  const box3 = new THREE.Mesh(box3Geometry,box3Material);
+  box3.position.x = 10;
+  box3.position.y = 4;
+  scene.add(box3);
 
   // カメラの設定
   const camera = new THREE.PerspectiveCamera(
     60, window.innerWidth/window.innerHeight, 0.1, 1000);
-  camera.position.x = 25;
+  camera.position.x = -10;
   camera.position.y = 15;
-  camera.position.z = 30;
+  camera.position.z = -30;
   camera.lookAt(0, 4, 0);
+
+  //光源の設定
+  const spotLight = new THREE.SpotLight(0xffffff,1000);
+  spotLight.position.set(-24,20,10);
+  scene.add(spotLight);
 
   // レンダラの設定
   const renderer = new THREE.WebGLRenderer();
